0fad9061a81e368e53d250c5303207b486e82202
GolubevDA <999gothicman999@mail.ru>
2024-03-29 16:22:36 +0700

    Добавление выборки Неполадок на странице создания заявки

---
+++ it_profi/main/management/commands/del.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+from main.models import Order
+
+
+#  Удалить заявку
+class Command(BaseCommand):
+    def handle(self, *args, **option):
+        order = Order.objects.order_by('id')
+        for el in order:
+            print(str(el.id) + ' - ' + str(el.client) + ' - ' + str(el.phone) + ' - ' + str(el.text))
