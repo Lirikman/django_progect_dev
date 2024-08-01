bb12e27abfe197c503ae64c57eaccaa2f7203774
GolubevDA <999gothicman999@mail.ru>
2024-08-01 16:29:29 +0700

    Add Django Rest framework. Add custom permission. Updating readme.

---
+++ it_profi/main/permission.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+
+# Разрешение удаления записи только Администратору, а просмотр всем
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_staff)
+
+
+# Разрешение изменение записи только автору статьи и Администратору
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user or request.user.is_staff
